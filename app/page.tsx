d4966bfca76c32b737717f95519beff998e44cbf
Afsana012 <dilafsana0@gmail.com>
2026-05-11 11:44:41 -0600

    Add Navbar component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="mt-4 text-lg">Get started by editing <code>app/page.tsx</code></p>
-    </main>
+   <>
+   <Navbar/>
+   </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-[#f4faf7] text-slate-900 p-2">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex items-center gap-2">
+            <img src="./images/logotb.png" alt="logo" className="w-60 h-auto object-contain"/>
+        </div>
+
+        <div className="flex items-center gap-6">
+          <a href="#" className="font-medium hover:text-blue-600 transition">Login</a>
+          <button className="bg-black text-white px-5 py-2 rounded-lg font-medium hover:bg-gray-800 transition">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
